95028337201d7a9817c3999efea16ca3d5205fcf
Stabien <bastien.piedallu@gmail.com>
2019-10-07 20:02:28 +0200

    little update on js

--- scroll.js
+++ scroll.js
@@ -7,6 +7,23 @@ var nav_link = document.getElementsByClassName("nav_onglet");
 var nav = document.getElementById("nav_fixed_scroll");
 var menu = document.getElementById("menu_deroulant");
 var menu_banner = document.getElementById("menu_deroulant_banniere");
+var sections = document.getElementsByTagName('section');
+
+/* Definition var height section */
+
+var height_section = [0, 0, 0, 0, 0];
+
+for (var i = 0; i < 5; i++) {
+    if (i === 0) {
+        height_section[i] = 530 + sections[0].clientHeight;
+    }
+    else {
+        height_section[i] = height_section[i - 1] + sections[i].clientHeight;
+    }
+}
+
+
+/* end of definition */
 
 window.addEventListener('scroll', function() {
     if (window.scrollY > 200) {
@@ -27,7 +44,7 @@ window.addEventListener('scroll', function() {
         nav_link[0].style.borderBottom = "4px solid #0282ce";
         nav_link[0].style.color = "#0282ce";
     }
-    if (window.scrollY > 530 && window.scrollY < 1295) {
+    if (window.scrollY > 530 && window.scrollY < height_section[0]) {
         for (var i = 0; i < 6; i++) {
             nav_link[i].style.borderBottom = "";
             nav_link[i].style.color = "";
@@ -35,7 +52,7 @@ window.addEventListener('scroll', function() {
         nav_link[1].style.borderBottom = "4px solid #0282ce";
         nav_link[1].style.color = "#0282ce";
     }
-    if (window.scrollY > 1295 && window.scrollY < 1945) {
+    if (window.scrollY > height_section[0] && window.scrollY < height_section[1]) {
         for (var i = 0; i < 6; i++) {
             nav_link[i].style.borderBottom = "";
             nav_link[i].style.color = "";
@@ -43,7 +60,7 @@ window.addEventListener('scroll', function() {
         nav_link[2].style.borderBottom = "4px solid #0282ce";
         nav_link[2].style.color = "#0282ce";
     }
-    if (window.scrollY > 1945 && window.scrollY < 2645) {
+    if (window.scrollY > height_section[1] && window.scrollY < height_section[2]) {
         for (var i = 0; i < 6; i++) {
             nav_link[i].style.borderBottom = "";
             nav_link[i].style.color = "";
@@ -51,7 +68,7 @@ window.addEventListener('scroll', function() {
         nav_link[3].style.borderBottom = "4px solid #0282ce";
         nav_link[3].style.color = "#0282ce";
     }
-    if (window.scrollY > 2645 && window.scrollY < 3295) {
+    if (window.scrollY > height_section[2] && window.scrollY < height_section[3]) {
         for (var i = 0; i < 6; i++) {
             nav_link[i].style.borderBottom = "";
             nav_link[i].style.color = "";
@@ -59,7 +76,7 @@ window.addEventListener('scroll', function() {
         nav_link[4].style.borderBottom = "4px solid #0282ce";
         nav_link[4].style.color = "#0282ce";
     }
-    if (window.scrollY > 3295) {
+    if (window.scrollY > height_section[3] - 200) {
         for (var i = 0; i < 6; i++) {
             nav_link[i].style.borderBottom = "";
             nav_link[i].style.color = "";
